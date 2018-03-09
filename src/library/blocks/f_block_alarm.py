6abdd905cdb916c6f22a9d1870f687ce165c4393
idilsuerdenlig <su.erdenlig@gmail.com>
2018-03-09 15:26:19 +0100

    f_block alarm output seperated

--- src/library/blocks/f_block_alarm.py
+++ src/library/blocks/f_block_alarm.py
@@ -2,22 +2,20 @@ from block import Block
 import numpy as np
 from memory_profiler import profile
 
-class fBlock(Block):
+
+class fBlockAlarm(Block):
 
     def __init__(self, phi, name=None):
-
         self.phi = phi
         super(fBlock, self).__init__(name=name)
 
     def _call(self, inputs, reward, absorbing, last, learn_flag):
-
-        self.last_output = self.phi(inputs)
-        self.alarm_output = None
-
+        self.last_output = None
+        self.alarm_output = self.phi(inputs)
 
     def reset(self, inputs):
-        self.last_output = self.phi(inputs)
-        self.alarm_output = None
+        self.last_output = None
+        self.alarm_output = self.phi(inputs)
 
     def init(self):
         self.last_output = None
