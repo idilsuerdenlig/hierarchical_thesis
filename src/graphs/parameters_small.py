8951de259800e7d3922bcc21092f98916b4bff23
Boris_il_forte <davide.tateo90@gmail.com>
2018-03-31 19:29:54 +0200

    Work on graphs
    - modified big
    - modified small
    - added stub for parameters_small

--- src/graphs/parameters_small.py
+++ src/graphs/parameters_small.py
@@ -7,10 +7,44 @@ from tqdm import tqdm, trange
 from matplotlib2tikz import save as tikz_save
 
 
+def compute_episode_lenght(dataset_eval, n_runs, ep_per_run):
+    size_eps_avg = np.zeros(n_runs+1)
+    size_eps = list()
+    ep_step_no = 0
+    for dataset_step in dataset_eval:
+        if not dataset_step[-1]:
+            ep_step_no += 1
+        else:
+            size_eps.append(ep_step_no)
+            ep_step_no = 0
+
+    for i in range(n_runs+1):
+        size_eps_avg[i] = np.mean(size_eps[ep_per_run * i:ep_per_run * i + ep_per_run], axis=0)
+
+    return size_eps_avg
+
+def compute_mean_J(dataset_eval, n_runs, ep_per_run, gamma):
+    J_runs_eps = compute_J(dataset_eval, gamma)
+    J_avg = np.zeros(n_runs+1)
+    for i in range(n_runs+1):
+        J_avg[i] = np.mean(J_runs_eps[ep_per_run * i:ep_per_run * i + ep_per_run], axis=0)
+
+    return J_avg
+
+
+def get_mean_and_confidence(data):
+    mean = np.mean(data, axis=0)
+    se = st.sem(data, axis=0)
+    n = len(data)
+
+    interval, _ = st.t.interval(0.95, n-1, scale=se)
+
+    return mean, interval
+
 
 def compute_data():
     base_dir = '/home/dave/Documenti/results_idil/Small/'
-    algorithms = ['H-GPOMDP', 'H-PGPE', 'H-PI']
+    algorithms = ['GPOMDP', 'H-GPOMDP', 'H-PGPE', 'H-PI', 'PGPE', 'REPS', 'RWR']
 
     len_results = dict()
     j_results = dict()
@@ -29,8 +63,9 @@ def compute_data():
         Lep = list()
 
         for exp_no in trange(how_many):
-            dataset_params = np.load(dir + str(exp_no) + '/parameter_dataset1_file.npy')
-
+            dataset_eval = np.load(dir + str(exp_no) + '/dataset_eval_file.npy')
+            J_avg = compute_mean_J(dataset_eval, n_runs, ep_per_run, 0.99)
+            L_avg = compute_episode_lenght(dataset_eval, n_runs, ep_per_run)
 
             Jep.append(J_avg)
             Lep.append(L_avg)
