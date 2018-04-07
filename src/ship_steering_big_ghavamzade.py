850c6aae35db42b22fd8a450cc89c237acb4c0c5
idilsuerdenlig <su.erdenlig@gmail.com>
2018-04-07 20:26:53 +0200

    low level tiles increased

--- src/ship_steering_big_ghavamzade.py
+++ src/ship_steering_big_ghavamzade.py
@@ -117,7 +117,7 @@ def experiment_ghavamzade(alg_high, alg_low, params, subdir, i):
     n_tiles = [5, 5, 10]
     low = np.array(low, dtype=np.float)
     high = np.array(high, dtype=np.float)
-    n_tilings = 1
+    n_tilings = 3
 
     tilingsL= Tiles.generate(n_tilings=n_tilings, n_tiles=n_tiles,
                              low=low, high=high)
@@ -242,7 +242,7 @@ def experiment_ghavamzade(alg_high, alg_low, params, subdir, i):
     function_block4.add_input(function_block6)
     function_block4.add_input(reward_acc_H)
 
-    function_block5.add_input(reward_ph)
+    #function_block5.add_input(reward_ph)
     function_block5.add_input(function_block7)
 
     function_block6.add_input(reward_ph)
@@ -320,7 +320,7 @@ if __name__ == '__main__':
     learning_rate_low = AdaptiveParameter(value=1e-2)
     n_jobs=1
     how_many = 1
-    n_runs = 100
+    n_runs = 50
     n_iterations = 20
     ep_per_run = 40
     low_ep_per_fit = 50
