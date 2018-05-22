293ead8a0b2fa1099a721a4a28b316b4ba0c12ad
boris-il-forte <boris.ilpossente@hotmail.it>
2018-05-22 18:44:18 +0200

    Fixed segway hierarchical
    - fixed all errors
    - now runs correctly

--- src/segway_hierarchical.py
+++ src/segway_hierarchical.py
@@ -1,4 +1,3 @@
-import datetime
 from joblib import Parallel, delayed
 
 from mushroom.utils import spaces
@@ -11,7 +10,6 @@ from mushroom.distributions import GaussianDiagonalDistribution
 from mushroom.approximators.parametric import LinearApproximator
 from mushroom.approximators.regressor import Regressor
 from mushroom.utils.dataset import compute_J
-from mushroom.utils.folder import *
 from mushroom.policy import DeterministicPolicy
 
 from mushroom.utils.parameters import AdaptiveParameter
@@ -22,10 +20,8 @@ from library.blocks.computational_graph import ComputationalGraph
 from library.blocks.control_block import ControlBlock
 from library.blocks.basic_operation_block import *
 from library.blocks.model_placeholder import PlaceHolder
-from library.blocks.reward_accumulator import reward_accumulator_block
 from library.blocks.functions.pick_first_state import pick_first_state
 from library.blocks.functions.fall_reward import fall_reward
-from library.blocks.functions.saturation import saturation
 from library.blocks.functions.angle_to_angle_diff_complete_state \
     import angle_to_angle_diff_complete_state
 from library.blocks.functions.lqr_cost_segway import lqr_cost_segway
@@ -67,9 +63,6 @@ def segway_experiment(alg_high, alg_low, params_high, params_low, subdir, i):
     # Function Block 5
     function_block5 = fBlock(name='f5 (fall punish low level)', phi=fall_reward)
 
-    # Function Block 6
-    #function_block6 = fBlock(name='f6 (saturation)', phi=saturation)
-
 
     # Features
     features1 = Features(basis_list=PolynomialBasis.generate(3,1)[1:])
@@ -86,14 +79,13 @@ def segway_experiment(alg_high, alg_low, params_high, params_low, subdir, i):
 
 
     # Agent 1
-    eps1 = 5e-2    #params.get('eps')
     lim = np.pi/2
     mdp_info_agent1 = MDPInfo(observation_space=mdp.info.observation_space,
                               action_space=spaces.Box(-lim, lim, (1,)),
                               gamma=mdp.info.gamma,
                               horizon=mdp.info.horizon)
 
-    agent1 = alg_high(dist1, pi1, mdp_info_agent1, **params_high)
+    agent_high = alg_high(dist1, pi1, mdp_info_agent1, **params_high)
 
     # Policy 2
     basis = PolynomialBasis.generate(1, 3)
@@ -115,47 +107,37 @@ def segway_experiment(alg_high, alg_low, params_high, params_low, subdir, i):
         action_space=mdp.info.action_space,
         gamma=mdp.info.gamma, horizon=300)
 
-    agent2 = alg_low(dist2, pi2, features2,
-                     mdp_info_agent2, **params_low)
+    print(params_low)
+
+    agent_low = alg_low(dist2, pi2, mdp_info_agent2,
+                     features=features2,**params_low)
 
     # Control Block 1
     parameter_callback1 = CollectDistributionParameter(dist1)
-    control_block1 = ControlBlock(name='Control Block High', agent=agent1,
+    control_block1 = ControlBlock(name='Control Block High', agent=agent_high,
                                   n_eps_per_fit=n_ep_per_fit,
                                   callbacks=[parameter_callback1])
 
     # Control Block 2
     parameter_callback2 = CollectDistributionParameter(dist2)
-    control_block2 = ControlBlock(name='Control Block Low', agent=agent2,
+    control_block2 = ControlBlock(name='Control Block Low', agent=agent_low,
                                   n_eps_per_fit=n_ep_per_fit,
                                   callbacks=[parameter_callback2])
-    control_block1.set_mask(n_eps=24)
-
-
-    #Reward Accumulator
-    #reward_acc = reward_accumulator_block(gamma=mdp_info_agent1.gamma,
-    #                                      name='reward_acc')
-
+    control_block1.set_mask()
 
     # Algorithm
     blocks = [state_ph, reward_ph, lastaction_ph, control_block1,
               control_block2, function_block1, function_block2,
               function_block3, function_block4, function_block5]
-              #, function_block6#, reward_acc]
 
     state_ph.add_input(control_block2)
     reward_ph.add_input(control_block2)
     lastaction_ph.add_input(control_block2)
-    #reward_acc.add_input(reward_ph)
-    #reward_acc.add_alarm_connection(control_block2)
     control_block1.add_input(function_block1)
-    #control_block1.add_reward(reward_acc)
     control_block1.add_reward(reward_ph)
-    #control_block1.add_alarm_connection(control_block2)
     control_block2.add_input(function_block2)
     control_block2.add_reward(function_block4)
     function_block1.add_input(state_ph)
-    #function_block2.add_input(function_block6)
     function_block2.add_input(control_block1)
 
     function_block2.add_input(state_ph)
@@ -163,61 +145,36 @@ def segway_experiment(alg_high, alg_low, params_high, params_low, subdir, i):
     function_block5.add_input(state_ph)
     function_block4.add_input(function_block3)
     function_block4.add_input(function_block5)
-    #function_block6.add_input(control_block1)
     computational_graph = ComputationalGraph(blocks=blocks, model=mdp)
     core = HierarchicalCore(computational_graph)
 
     # Train
-    low_level_dataset_eval = list()
-    dataset_eval = list()
-
     dataset_eval_run = core.evaluate(n_episodes=eval_run, render=True)
     J = compute_J(dataset_eval_run, gamma=mdp.info.gamma)
     print('J at start : ' + str(np.mean(J)))
-    dataset_eval += dataset_eval_run
 
     mask_done = False
     for n in range(n_epochs):
         print('ITERATION', n)
         if n == 0:
             control_block1.set_mask()
-            n_weights_h = alg_high.policy._approximator.weights_size
-            dist1._mu = np.zeros(n_weights_h)
-            dist1._sigma = 1e-8 * np.ones(n_weights_h)
+            dist1.set_parameters(np.array([0, 1e-8]))
 
-        if n > 0 and not mask_done:
+        if n > 20 and not mask_done:
             control_block1.unset_mask()
-            dist1._mu = np.zeros(n_weights_h)
-            dist1._sigma = 1 * np.ones(n_weights_h)
+            dist1.set_parameters(np.array([0, 1e0]))
             mask_done = True
 
         core.learn(n_episodes=n_iterations*n_ep_per_fit, skip=True)
         dataset_eval_run = core.evaluate(n_episodes=eval_run, render=True)
-        dataset_eval += dataset_eval_run
         J = compute_J(dataset_eval_run, gamma=mdp.info.gamma)
         print('J at iteration ' + str(n) + ': ' + str(np.mean(J)))
         print('dist H:', dist1.get_parameters())
         print('dist L mu:', dist2.get_parameters()[:3])
         print('dist L sigma:', dist2.get_parameters()[3:])
-        #low_level_dataset_eval += control_block2.dataset.get()
-
-
-    # Save
-    #parameter_dataset1 = parameter_callback1.get_values()
-    #parameter_dataset2 = parameter_callback2.get_values()
-
-    #mk_dir_recursive('./' + subdir + str(i))
-
-    #np.save(subdir+str(i)+'/low_level_dataset_file', low_level_dataset_eval)
-    #np.save(subdir+str(i)+'/parameter_dataset1_file', parameter_dataset1)
-    #np.save(subdir+str(i)+'/parameter_dataset2_file', parameter_dataset2)
-    #np.save(subdir+str(i)+'/dataset_eval_file', dataset_eval)
 
 
 if __name__ == '__main__':
-
-    #subdir = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S') + \
-    #         '_segway_hierarchical/'
     alg_high = REPS
     alg_low = REPS
     learning_rate_high = AdaptiveParameter(value=50)
@@ -230,8 +187,10 @@ if __name__ == '__main__':
     algs_params = [
             (REPS, REPS, {'eps': eps_high}, {'eps': eps_low}),
             (RWR, REPS, {'beta': beta_high}, {'beta': beta_low}),
-            (PGPE, PGPE, {'learning_rate': learning_rate_high}, {'learning_rate': learning_rate_low}),
+            (PGPE, PGPE, {'learning_rate': learning_rate_high},
+             {'learning_rate': learning_rate_low}),
         ]
+
     n_jobs = 1
     how_many = 1
     n_epochs = 20
@@ -239,16 +198,14 @@ if __name__ == '__main__':
     n_ep_per_fit = 25
     eval_run = 10
 
-    #mk_dir_recursive('./' + subdir)
-    #force_symlink('./' + subdir, 'latest')
-    #np.save(subdir + '/algorithm_params_dictionary', params)
+
     experiment_params = {'how_many': how_many,
                          'n_epochs': n_epochs,
                          'n_iterations': n_iterations,
                          'n_ep_per_fit': n_ep_per_fit,
                          'eval_run': eval_run}
-    #np.save(subdir + '/experiment_params_dictionary', experiment_params)
     for alg_high, alg_low, params_high, params_low in algs_params:
         Js = Parallel(n_jobs=n_jobs)(delayed(segway_experiment)
-                                 (alg_high, alg_low, params_high, params_low, None, i)
+                                 (alg_high, alg_low, params_high, params_low,
+                                  None, i)
                                  for i in range(how_many))
