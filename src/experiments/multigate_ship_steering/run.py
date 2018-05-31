61f91b243dbb61a23e0ac1856a3c7f76ab135fbd
idilsuerdenlig <su.erdenlig@gmail.com>
2018-05-31 15:14:12 +0200

    multigate experiment change

--- src/experiments/multigate_ship_steering/run.py
+++ src/experiments/multigate_ship_steering/run.py
@@ -2,7 +2,8 @@ import datetime
 from joblib import Parallel, delayed
 
 from mushroom.algorithms.policy_search import *
-from mushroom.utils.parameters import AdaptiveParameter
+from mushroom.algorithms.value.td import *
+from mushroom.utils.parameters import AdaptiveParameter, Parameter
 from mushroom.utils.folder import *
 
 from mushroom_hierarchical.environments.ship_steering_multiple_gates import *
@@ -37,14 +38,27 @@ if __name__ == '__main__':
 
     # Hierarchical
     algs_and_params_hier = [
-        (GPOMDP, {'learning_rate': AdaptiveParameter(value=50)},
-         PGPE, {'learning_rate': AdaptiveParameter(value=5e-4)})
-    ]
+        (QLearning, {'learning_rate': Parameter(value=10)},
+        GPOMDP, {'learning_rate': AdaptiveParameter(value=50)},
+        GPOMDP, {'learning_rate': AdaptiveParameter(value=50)},
+        GPOMDP, {'learning_rate': AdaptiveParameter(value=50)},
+        GPOMDP, {'learning_rate': AdaptiveParameter(value=50)},
+        PGPE, {'learning_rate': AdaptiveParameter(value=5e-4)})
+         ]
 
-    for alg_h, params_h, alg_l, params_l in algs_and_params_hier:
-        mu = 500
-        sigma = 250
-        agent_h = build_high_level_agent(alg_h, params_h, mdp, mu, sigma)
+    for alg_h, params_h, alg_m1, params_m1, alg_m2, params_m2,\
+        alg_m3, params_m3, alg_m4, params_m4, alg_l, params_l in algs_and_params_hier:
+
+        epsilon = Parameter(value=0.15)
+        agent_h = build_high_level_agent(alg_h, params_h, mdp, epsilon)
+
+        mu1 = 500; mu2 = 500; mu3 = 500; mu4 = 500
+        sigma1 = 250; sigma2 = 250; sigma3 = 250; sigma4 = 250
+        agent_m1 = build_mid_level_agent(alg_m1, params_m1, mdp, mu, sigma)
+        agent_m2 = build_mid_level_agent(alg_m2, params_m2, mdp, mu, sigma)
+        agent_m3 = build_mid_level_agent(alg_m3, params_m3, mdp, mu, sigma)
+        agent_m4 = build_mid_level_agent(alg_m4, params_m4, mdp, mu, sigma)
+
         agent_l = build_low_level_agent(alg_l, params_l, mdp)
 
         ep_per_run_hier = ep_per_epoch // n_iterations_hier
