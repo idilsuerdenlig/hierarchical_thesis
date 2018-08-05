e40b69ed579cc1d17ad636c3d4b6551b5ffa060a
Boris_il_forte <davide.tateo90@gmail.com>
2018-08-05 16:03:19 +0200

    Updated file plot

--- src/graphs/plot_ship_steering_small.py
+++ src/graphs/plot_ship_steering_small.py
@@ -4,24 +4,27 @@ import numpy as np
 from common import *
 
 
-base_dir = '/media/dave/af351f33-87a3-4142-a426-ad42d5930c4e/hierarchical/ship_steering_small_2018-07-25_14-07-57'
-base_dir = '/home/dave'
-algs = ['GPOMDP']
-''',
+base_dir = '../experiments/ship_steering/ship_steering_small_latest'
+algs = ['GPOMDP',
         'PGPE',
         'REPS',
         'RWR',
-        'H_GPOMDP_PGPE']'''
+        'H_GPOMDP_PGPE']
 
 colors = ['b', 'r', 'g', 'c', 'm']
 
 J_results = dict()
+L_results = dict()
 
 for alg in algs:
-    J = np.load(base_dir + '/' + alg + '.npy')
+    J = np.load(base_dir + '/J_' + alg + '.npy')
     J_results[alg] = get_mean_and_confidence(J)
 
+    L = np.load(base_dir + '/L_' + alg + '.npy')
+    L_results[alg] = get_mean_and_confidence(L)
+
 create_plot(algs, colors, J_results, 'J', legend=True)
+create_plot(algs, colors, L_results, 'L', legend=True)
 
 plt.show()
 
