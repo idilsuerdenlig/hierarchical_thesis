cd9c65e708213f30dfc091c88601b884c286077c
Boris_il_forte <davide.tateo90@gmail.com>
2018-08-11 16:28:04 +0200

    Added new results
    - Added segway results for hierarchical reps + rwr
    - Added ghavamzadeh results in big environment

--- src/graphs/plot_ship_steering_big.py
+++ src/graphs/plot_ship_steering_big.py
@@ -4,13 +4,9 @@ import numpy as np
 from common import *
 
 
-base_dir = '../results/segway_2018-08-08_14-13-16'
-algs = [
-        'REPS',
-        'RWR',
-        'H_RWR_RWR',
-        'H_REPS_RWR']
-        #'H_GPOMDP_PGPE']
+base_dir = '../results/ship_steering_big_2018-08-06_16-23-52'
+algs = ['H_GPOMDP_PGPE',
+        'ghavamzadeh']
 
 colors = ['b', 'r', 'g', 'c', 'm']
 
@@ -20,12 +16,11 @@ L_results = dict()
 for alg in algs:
     J = np.load(base_dir + '/J_' + alg + '.npy')
     J_results[alg] = get_mean_and_confidence(J)
-    print(alg, ': ', J.shape)
 
     L = np.load(base_dir + '/L_' + alg + '.npy')
     L_results[alg] = get_mean_and_confidence(L)
 
-create_plot(algs, colors, J_results, 'J', legend=True, logarithmic=True)
+create_plot(algs, colors, J_results, 'J', legend=True)
 create_plot(algs, colors, L_results, 'L', legend=True)
 
 plt.show()
